f09a12cc6d15036185241f99677750ef5c16f8b3
Peter Goes <petergoes@gmail.com>
2016-09-15 17:06:50 +0200

    refactor(renderer): make body droppable

--- ui/renderer.js
+++ ui/renderer.js
@@ -1,4 +1,22 @@
 const { ipcRenderer } = require('electron');
+const fs = require('fs');
 
 console.log('send ping');
 ipcRenderer.send('ping');
+
+window.ondrop = function ondrop(event) {
+	event.preventDefault();
+	const files = event.dataTransfer.files;
+	const file = files[0] || {};
+	const fileStats = fs.statSync(file.path);
+
+	console.log('path:', file.path);
+	console.log('is folder:', fileStats.isDirectory());
+};
+
+function preventDefault(event) {
+	event.preventDefault();
+}
+
+window.ondragover = preventDefault;
+window.ondragleave = preventDefault;
