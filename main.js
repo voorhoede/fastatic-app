fd9b04ac7a8dc659c49fc732b293666b4652f668
Peter Goes <petergoes@gmail.com>
2016-10-13 16:38:34 +0200

    docs(fastatic-runner): document code

--- main.js
+++ main.js
@@ -3,41 +3,24 @@ const electron = require('electron');
 const devtoolsInstaller = require('electron-devtools-installer');
 const { REDUX_DEVTOOLS } = require('electron-devtools-installer');
 const fastaticRunner = require('./lib/fastatic-runner').default;
-// Module to control application life.
-const app = electron.app;
-// Module to create native browser window.
-const BrowserWindow = electron.BrowserWindow;
-// Module to communicate between the window and app
-const ipcMain = electron.ipcMain;
 
-// Keep a global reference of the window object, if you don't, the window will
-// be closed automatically when the JavaScript object is garbage collected.
+const app = electron.app;
+const BrowserWindow = electron.BrowserWindow;
+const ipcMain = electron.ipcMain;
 let mainWindow;
 
 devtoolsInstaller.default(REDUX_DEVTOOLS);
 
 function createWindow() {
-	// Create the browser window.
 	mainWindow = new BrowserWindow({ width: 800, height: 600, x: 0, y: 0, titleBarStyle: 'hidden-inset' });
-
-	// and load the index.html of the app.
 	mainWindow.loadURL(`file://${__dirname}/ui/index.html`);
-
-	// Open the DevTools.
 	mainWindow.webContents.openDevTools();
 
-	// Emitted when the window is closed.
 	mainWindow.on('closed', () => {
-		// Dereference the window object, usually you would store windows
-		// in an array if your app supports multi windows, this is the time
-		// when you should delete the corresponding element.
 		mainWindow = null;
 	});
 }
 
-// This method will be called when Electron has finished
-// initialization and is ready to create browser windows.
-// Some APIs can only be used after this event occurs.
 app.on('ready', createWindow);
 
 // Quit when all windows are closed.
@@ -57,9 +40,10 @@ app.on('activate', () => {
 	}
 });
 
-// In this file you can include the rest of your app's specific main process
-// code. You can also put them in separate files and require them here.
-
+//
+// Setup communication between the window and this main process.
+// The actual running of the fastatic module is delegated to the fastaticRunner
+// module to keep this main module slim.
 ipcMain.on('run-fastatic', (event, args) => {
 	event.sender.send('fastatic-is-running');
 
