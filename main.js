53513ee851d431f47d9736779e419db4cf3adeba
Peter Goes <petergoes@gmail.com>
2016-11-11 17:36:42 +0100

    refactor(main): add Menu bar

--- main.js
+++ main.js
@@ -1,24 +1,27 @@
 require('nodejs-dashboard');
-const electron = require('electron');
+const { app, Menu, BrowserWindow, ipcMain, dialog } = require('electron');
 const devtoolsInstaller = require('electron-devtools-installer');
 const { REDUX_DEVTOOLS } = require('electron-devtools-installer');
 const FastaticRunner = require('./lib/fastatic-runner');
 
-const app = electron.app;
-const BrowserWindow = electron.BrowserWindow;
-const ipcMain = electron.ipcMain;
 let mainWindow;
 let fastaticRunnerInstance;
+let menuTemplate;
+let menu;
 
 devtoolsInstaller.default(REDUX_DEVTOOLS);
 
 function createWindow() {
 	mainWindow = new BrowserWindow({ width: 800, height: 600, x: 0, y: 0, titleBarStyle: 'hidden-inset' });
 	mainWindow.loadURL(`file://${__dirname}/ui/index.html`);
+	mainWindow.webContents.openDevTools();
 
 	mainWindow.on('closed', () => {
 		mainWindow = null;
 	});
+
+	menu = Menu.buildFromTemplate(menuTemplate);
+	Menu.setApplicationMenu(menu);
 }
 
 app.on('ready', createWindow);
@@ -48,6 +51,9 @@ ipcMain.on('run-fastatic', (event, args) => {
 	event.sender.send('fastatic-is-running');
 	fastaticRunnerInstance = new FastaticRunner();
 
+	menu.items[1].submenu.items[0].enabled = false;
+	menu.items[1].submenu.items[1].enabled = true;
+
 	fastaticRunnerInstance.runFastatic(args.src)
 		.then(output => event.sender.send('fastatic-is-completed', output))
 		.catch(errors => {console.trace(errors); event.sender.send('fastatic-failed', errors)});
@@ -58,5 +64,51 @@ ipcMain.on('destination-chosen', (event, args) => {
 
 
 	fastaticRunnerInstance.setDestination(args.dest)
-		.then(dest => event.sender.send('flow-finished', { dest }));
+		.then(dest => event.sender.send('flow-finished', { dest }))
+		.then(() => {
+			menu.items[1].submenu.items[0].enabled = true;
+			menu.items[1].submenu.items[1].enabled = false;
+		});
 });
+
+
+menuTemplate = [
+	{
+		submenu: [
+			{ role: 'about' },
+			{ type: 'separator' },
+			{ role: 'hide' },
+			{ type: 'separator' },
+      		{ role: 'quit' }
+		]
+	},
+	{
+		label: 'Edit',
+		submenu: [
+			{
+				label: 'Open Source dir',
+				accelerator: 'CmdOrCtrl+o',
+				click (item, focusedWindow) {
+					dialog.showOpenDialog(mainWindow, { properties: ['openDirectory'] }, (paths) => {
+						if (paths && paths[0]) {
+							focusedWindow.webContents.send('menu-open-source', paths[0]);
+						}
+					});
+				},
+				enabled: true
+			},
+			{
+				label: 'Open Dest dir',
+				accelerator: 'CmdOrCtrl+d',
+				click (item, focusedWindow) {
+					dialog.showOpenDialog(mainWindow, { properties: ['openDirectory'] }, (paths) => {
+						if (paths && paths[0]) {
+							focusedWindow.webContents.send('menu-open-dest', paths[0]);
+						}
+					});
+				},
+				enabled: false
+			}
+		]
+	}
+];
--- ui/index.js
+++ ui/index.js
@@ -2,10 +2,6 @@ const store = require('./store');
 const path = require('path');
 const { ipcRenderer } = require('electron');
 
-store.subscribe(() => {
-	const state = store.getState();
-});
-
 ipcRenderer.on('fastatic-is-running', () => {
 	store.dispatch({ type: 'RUN_FASTATIC' });
 	store.dispatch({ type: 'DROPZONE_SRC_DISABLED', value: true });
@@ -29,27 +25,26 @@ ipcRenderer.on('destination-accepted', () => {
 	store.dispatch({ type: 'DROPZONE_DEST_DISABLED', value: true });
 });
 
-
 ipcRenderer.on('flow-finished', (event, args) => {
 	store.dispatch({ type: 'SET_RESULT_DEST_PATH', value: args.dest });
 	store.dispatch({ type: 'STOP_FASTATIC' });
 	store.dispatch({ type: 'SHOW_RESULT' });
 });
 
-function formatError(error) {
-	const { cwd, fileName } = error || {};
-	const { line, col, message } = error.cause || {};
-	const where = (cwd && fileName) ? `${path.relative(cwd, fileName)}:${line}:${col}` : null;
-	const stack = (error.stack) ? error.stack.split('\n    ').slice(1).join('\n\t ') : '';
-
-	const format = {
-		name: error.name || 'Parser Error',
-		parser: error.parser,
-		plugin: error.plugin,
-		message: message || error.message,
-		where: where || stack,
-		show: true
+ipcRenderer.on('menu-open-source', (event, args) => {
+	const actionPath = {
+		type: `dropzone_src_path`.toUpperCase(),
+		path: args,
+		isFile: false
 	};
+	store.dispatch(actionPath);
+});
 
-	return format;
-}
+ipcRenderer.on('menu-open-dest', (event, args) => {
+	const actionPath = {
+		type: `dropzone_dest_path`.toUpperCase(),
+		path: args,
+		isFile: false
+	};
+	store.dispatch(actionPath);
+});
